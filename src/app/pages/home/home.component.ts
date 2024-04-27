4175b1598fd17d9b84201a2541c78a502bc9ee1e
artsol0 <artur200360@gmail.com>
2024-04-27 11:24:03 +0300

    Implemented pagination. Design improved.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatTabsModule, MatIconModule, MatButtonModule],
+  imports: [MatTabsModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { SnackbarService } from '../../services/snackbar/snackbar.service';
 import { BookService } from '../../services/book/book.service';
+import { BookAttributesService } from '../../services/attributes/book-attributes.service';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatIconModule, RouterModule, MatCardModule, MatPaginatorModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatSidenavModule, MatSelectModule, MatIconModule, RouterModule, MatCardModule, MatPaginatorModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
@@ -22,20 +26,98 @@ export class SearchComponent implements OnInit {
 
   books:any = [];
   page:number = 0;
+  pageEvent: PageEvent = new PageEvent;
   totalBooks = 0;
   totalPages = 0;
   pageSize = 0;
+
   errorMessage = '';
   reponseMessage:any;
+
+  mobileQuery: MediaQueryList;
+  _mobileQueryListener: () => void;
+
+  genres: { id: number, genre: string }[] = [];
+  languages: { id: number, language: string }[] = [];
+  qualities: { id: number, quality: string }[] = [];
+  statuses: { id: number, status: string }[] = [];
+  selectedGenres:number[] = [];
 
   constructor (
     private route: ActivatedRoute,
     private router: Router, 
     private bookService: BookService,
-    private snackbarService: SnackbarService) {}
+    private attributesService:BookAttributesService,
+    private snackbarService: SnackbarService,
+    changeDetectorRef: ChangeDetectorRef, 
+    media: MediaMatcher) {
+      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+    }
 
   ngOnInit(): void {
     this.getAllBooks(this.page,'');
+
+    this.attributesService.getGenres().subscribe({
+      next: data => {
+        this.genres = data.data;
+      },
+      error: (error: any) => {
+       if (error.error?.error.message) {
+          this.reponseMessage = error.error?.error.message;
+        } else {
+          this.reponseMessage = "Unexpected error occurred";
+        }
+        this.snackbarService.openSnackBar(this.reponseMessage, "error");
+      }
+    });
+
+    this.attributesService.getLanguages().subscribe({
+      next: data => {
+        this.languages = data.data;
+      },
+      error: (error: any) => {
+       if (error.error?.error.message) {
+          this.reponseMessage = error.error?.error.message;
+        } else {
+          this.reponseMessage = "Unexpected error occurred";
+        }
+        this.snackbarService.openSnackBar(this.reponseMessage, "error");
+      }
+    });
+
+    this.attributesService.getQualities().subscribe({
+      next: data => {
+        this.qualities = data.data;
+      },
+      error: (error: any) => {
+       if (error.error?.error.message) {
+          this.reponseMessage = error.error?.error.message;
+        } else {
+          this.reponseMessage = "Unexpected error occurred";
+        }
+        this.snackbarService.openSnackBar(this.reponseMessage, "error");
+      }
+    });
+
+    this.attributesService.getStatuses().subscribe({
+      next: data => {
+        this.statuses = data.data;
+      },
+      error: (error: any) => {
+       if (error.error?.error.message) {
+          this.reponseMessage = error.error?.error.message;
+        } else {
+          this.reponseMessage = "Unexpected error occurred";
+        }
+        this.snackbarService.openSnackBar(this.reponseMessage, "error");
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 
   getAllBooks(page:number, keyword:string) {
@@ -57,6 +139,11 @@ export class SearchComponent implements OnInit {
     })
   }
 
+  pageChanged(event: PageEvent){
+    this.page = event.pageIndex;
+    this.getAllBooks(this.page,'');
+  }
+
   searchByKeyword(searchkeyword:string) {
     this.page = 0;
     this.books = [];
@@ -65,6 +152,17 @@ export class SearchComponent implements OnInit {
 
   trackById(index: number, book: any): number {
     return book.id;
+  }
+
+  filterBookForm = new FormGroup({
+    genres: new FormControl(""),
+    language: new FormControl(""),
+    quality: new FormControl(""),
+    status: new FormControl(""),
+  });
+
+  handleFilterBooks() {
+
   }
 
 }
